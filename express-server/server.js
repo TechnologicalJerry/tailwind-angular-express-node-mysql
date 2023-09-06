1a301c8c36b2c7f7d20b8bfe52422132aa449e6c
technologicaljerry <cserajatverma@gmail.com>
2023-09-06 23:42:13 +0530

    Update server.js

--- express-server/server.js
+++ express-server/server.js
@@ -1,12 +1,12 @@
-import express from 'express'
+import express from 'express';
 
-import { getNotes, getNote, createNote } from './database.js'
+import { getNotes, getNote, createNote } from './database.js';
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
 app.get("/notes", async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
-})
+});
