c580d8e8b8fd3c1eda0b3d787229a3238c5c373b
technologicaljerry <cserajatverma@gmail.com>
2023-09-06 23:42:26 +0530

    Update server.js

--- express-server/server.js
+++ express-server/server.js
@@ -10,3 +10,9 @@ app.get("/notes", async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
 });
+
+app.get("/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const note = await getNote(id)
+    res.send(note)
+  })
