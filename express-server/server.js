ac123724c40879df0129c5d920546c4211f5b2ee
technologicaljerry <cserajatverma@gmail.com>
2023-09-06 23:43:42 +0530

    Update server.js

--- express-server/server.js
+++ express-server/server.js
@@ -28,3 +28,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke 💩')
 });
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+});
