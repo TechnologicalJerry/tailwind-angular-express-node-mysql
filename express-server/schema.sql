6f01dc56802549f76d204444398c10bfc69510a8
technologicaljerry <cserajatverma@gmail.com>
2023-09-07 23:20:46 +0530

    Create schema.sql

---
+++ express-server/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE notes_app;
+USE notes_app;
+
+CREATE TABLE notes (
+  id integer PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  contents TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
