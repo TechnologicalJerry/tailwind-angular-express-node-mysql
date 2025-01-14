07b205ad3dba3b7521c1cfb377b3ded8f5f87b50
technologicaljerry <cserajatverma@gmail.com>
2025-01-14 21:34:04 +0530

    Create default.ts

---
+++ express-server/config/default.ts
@@ -0,0 +1,14 @@
+export default {
+  port: 5050,
+  DB_HOST: "localhost",
+  DB_USER: "root",
+  DB_PASSWORD: "password",
+  DB_NAME: "my_database",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+};
