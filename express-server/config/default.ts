f5536836ab72817d9ba6416e694223c2e381648a
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-29 20:54:48 +0530

    Update database config to use MongoDB URI
    Replaces individual MySQL database parameters with a single MongoDB connection URI in the default configuration file.

--- express-server/config/default.ts
+++ express-server/config/default.ts
@@ -1,9 +1,6 @@
 export default {
   port: 5050,
-  DB_HOST: "localhost",
-  DB_USER: "root",
-  DB_PASSWORD: "password",
-  DB_NAME: "my_database",
+  dbUri: "mongodb://localhost:27017/my_database",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
