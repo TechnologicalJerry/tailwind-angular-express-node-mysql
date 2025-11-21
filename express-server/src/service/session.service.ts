7dd920ff40f6e3d96d2ad7ca3203ffb7b0dc4d80
Rajat Verma <cserajatverma@gmail.com>
2025-11-21 21:24:10 +0530

    Refactor session service for updated model fields
    Updated session service functions to use new model field names (user_id, user_agent, id) and adjusted query types for compatibility. This aligns the service with changes in the session model and improves type safety.

--- express-server/src/service/session.service.ts
+++ express-server/src/service/session.service.ts
@@ -1,23 +1,22 @@
 import { get } from "lodash";
 import config from "config";
-import { FilterQuery, UpdateQuery } from "mongoose";
 import SessionModel, { SessionDocument } from "../models/session.model";
 import { verifyJwt, signJwt } from "../utils/jwt.utils";
 import { findUser } from "./user.service";
 
-export async function createSession(userId: string, userAgent: string) {
-  const session = await SessionModel.create({ user: userId, userAgent });
+export async function createSession(userId: number, userAgent: string) {
+  const session = await SessionModel.create({ user_id: userId, user_agent: userAgent });
 
-  return session.toJSON();
+  return session;
 }
 
-export async function findSessions(query: FilterQuery<SessionDocument>) {
-  return SessionModel.find(query).lean();
+export async function findSessions(query: Partial<SessionDocument>) {
+  return SessionModel.findOne(query);
 }
 
 export async function updateSession(
-  query: FilterQuery<SessionDocument>,
-  update: UpdateQuery<SessionDocument>
+  query: Partial<SessionDocument>,
+  update: Partial<SessionDocument>
 ) {
   return SessionModel.updateOne(query, update);
 }
@@ -35,12 +34,12 @@ export async function reIssueAccessToken({
 
   if (!session || !session.valid) return false;
 
-  const user = await findUser({ _id: session.user });
+  const user = await findUser({ id: session.user_id });
 
   if (!user) return false;
 
   const accessToken = signJwt(
-    { ...user, session: session._id },
+    { ...user, session: session.id },
     "accessTokenPrivateKey",
     { expiresIn: config.get("accessTokenTtl") } // 15 minutes
   );
