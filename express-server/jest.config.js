d77ad314ac608039ddc1184a6e443c5f5936a230
Rajat Verma <cserajatverma@gmail.com>
2025-11-21 21:22:04 +0530

    Add Jest configuration for Express server
    Introduces jest.config.js to set up testing with ts-jest, specify test environment, coverage options, and test file patterns for the Express server project.

---
+++ express-server/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/server.ts', // Exclude server startup file from coverage
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testTimeout: 10000,
+};
