3592de7b9af2dac9a91baff6340da44d533dbe5d
Rajat Verma <cserajatverma@gmail.com>
2026-09-01 23:30:36 +0530

    feat: implement auth interceptor, user service, and CRUD product API endpoints

--- angular-client/src/app/core/interceptors/auth-interceptor.ts
+++ angular-client/src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authService = inject(AuthService);
+  const accessToken = authService.getAccessToken();
+  const refreshToken = authService.getRefreshToken();
+
+  let headers = req.headers;
+
+  if (accessToken) {
+    headers = headers.set('Authorization', `Bearer ${accessToken}`);
+  }
+  if (refreshToken) {
+    headers = headers.set('x-refresh', refreshToken);
+  }
+
+  const authReq = req.clone({ headers });
+  return next(authReq);
 };
+
